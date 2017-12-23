a1afd5d54ae241c85452c9878b8d3e57102b2316
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-12-23 15:49:37 +0100

    storage: made basic authentication configurable

--- src/storage/routes/auth.js
+++ src/storage/routes/auth.js
@@ -3,10 +3,14 @@ var BasicStrategy = require('passport-http').BasicStrategy;
 
 passport.use(new BasicStrategy(
   function(username, password, callback) {
-    if (username !== 'push') { return callback(null, false) }
-    if (password !== 'push') { return callback(null, false) }
+    if (username !== (process.env.API_USER || 'push') ) {
+      return callback(null, false)
+    }
+    if (password !== (process.env.API_PASSWORD || 'push') ) {
+      return callback(null, false)
+    }
     return callback(null, username);
-  } 
+  }
 ));
 
 exports.isAuthenticated = passport.authenticate('basic', { session : false });
