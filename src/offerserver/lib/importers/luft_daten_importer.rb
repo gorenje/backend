df15e35b9e8bb77b4a4625b302d4a33124b73856
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-12-29 14:01:54 +0100

    offerserver: made the luftdaten more explicit.

--- src/offerserver/lib/importers/luft_daten_importer.rb
+++ src/offerserver/lib/importers/luft_daten_importer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Import temperature, humdity, pressure and dirt-in-the-air measurements.
 #
 # For more details, see luftdaten.info
@@ -12,20 +13,34 @@ class LuftDatenImporter
 
   include BaseImporter
 
+  AcceptedValueTypes = ["temperature", "humidity", "p1", "p2"]
+
+  ValueTypeNames = {
+    "temperature" => "Temperature",
+    "humidity"    => "Humidity",
+    "p1"          => "Feinstaub (10 µg/m³)",
+    "p2"          => "Feinstaub (2.5 µg/m³)"
+  }
+
   def sensor_hash_to_readings(sensorhsh)
     sensorid = sensorhsh["sensor"]["id"]
     sensorhsh["sensordatavalues"].map do |sendata|
+      value_type = sendata["value_type"].downcase
+      next unless AcceptedValueTypes.include?(value_type)
+
+      extra_keywords = value_type =~ /^p/ ? ["#feinstaub"] : []
       OpenStruct.
         new({}.tap do |h|
               h["sid"]  = sensorid
-              h["id"]   = "%s:%s" % [sendata["value_type"], sensorid]
-              h["text"] = "%s: %s" % [sendata["value_type"].capitalize,
-                                      sendata["value"]]
+              h["id"]   = "%s:%s" % [value_type, sensorid]
+              h["text"] = "%s: %s" % [ValueTypeNames[value_type] ||
+                                      value_type.capitalize,
+                                      "%0.2f" % sendata["value"].to_f]
               h["lat"] = sensorhsh["location"]["latitude"]
               h["lng"] = sensorhsh["location"]["longitude"]
-              h["keywords"] = ["##{sendata["value_type"]}"].map(&:downcase)
+              h["keywords"] = ["##{value_type}"] + extra_keywords
             end)
-    end
+    end.compact
   end
 
   def perform
@@ -53,13 +68,16 @@ class LuftDatenImporter
           d["text"]                    = reading.text
           d["validuntil"]              = timestamp + TwentyMinutesMS
           d["location"]["coordinates"] = [reading.lng, reading.lat]
-          d["extdata"]                 = { :id => reading.id,
-                                           :sid =>  reading.sid }
+          d["extdata"]                 = { :id  => reading.id,
+                                           :sid => reading.sid }
           d["keywords"] = BaseKeyWords + reading.keywords
         end
       end
     end
 
-    send_off(new_offers, old_offers)
+    remove_offers, old_offers =
+      old_offers.partition { |offr| offr["validuntil"] < timestamp }
+
+    send_off(new_offers, old_offers, remove_offers)
   end
 end
