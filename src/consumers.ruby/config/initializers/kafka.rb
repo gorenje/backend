8767fef2b28a327bcab50a4e328ac23ca027db33
Gerrit Riessen <gerrit.riessen@gmail.com>
2018-01-18 15:48:09 +0100

    consumers.ruby: don't start the worker if kafka is not available

--- src/consumers.ruby/config/initializers/kafka.rb
+++ src/consumers.ruby/config/initializers/kafka.rb
@@ -8,14 +8,9 @@ opts = {
 }
 opts[:logger].level = Logger::WARN if ENV['RACK_ENV'] == 'production'
 
-$kafka = begin
-           OpenStruct.new.tap do |os|
-             ["geo", "kafidx", "chatbot", "bulkdata", "notifier"].
-               each do |client_id|
+$kafka = OpenStruct.new.tap do |os|
+           ["geo", "kafidx", "chatbot", "bulkdata", "notifier"].
+             each do |client_id|
                os[client_id] = Kafka.new(opts.merge({:client_id => client_id}))
              end
-           end
-         rescue Exception => e
-           puts("Unable to configure Kafka Client: #{e}")
-           OpenStruct.new
          end
--- src/consumers.ruby/config/initializers/zookeeper.rb
+++ src/consumers.ruby/config/initializers/zookeeper.rb
@@ -4,18 +4,13 @@ module ZkClient
   extend self
 
   def broker_list
-    begin
-      [].tap do |broker_list|
-        zk = ZK.new(ENV['ZOOKEEPER_HOST'])
-        zk.children("/brokers/ids").each do |broker_id|
-          data = JSON(zk.get("/brokers/ids/#{broker_id}").first)
-          broker_list << data["endpoints"]
-        end
-        zk.close
-      end.flatten
-    rescue Exception => e
-      puts "Not able to get broker list: #{e.message}"
-      []
-    end
+    [].tap do |broker_list|
+      zk = ZK.new(ENV['ZOOKEEPER_HOST'])
+      zk.children("/brokers/ids").each do |broker_id|
+        data = JSON(zk.get("/brokers/ids/#{broker_id}").first)
+        broker_list << data["endpoints"]
+      end
+      zk.close
+    end.flatten
   end
 end
